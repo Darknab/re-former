dcc84b6442d99d372fb3fa7e0e118cd2d14461f7
Darknab <darknab@live.fr>
2023-06-04 17:33:28 +0100

    Add a show article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/users", to: "users#index"
+  get"/users/:id", to: "users#show"
   
   resource :users, as: "users"
 
