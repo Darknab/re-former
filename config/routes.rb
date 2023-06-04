1b3910564796018f60468405d3a94122f1ad7e25
Darknab <darknab@live.fr>
2023-06-04 12:07:26 +0100

    Added an edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :users, only: [:new, :create]
+  resource :users, as: "users", only: [:new, :create]
+
+  get "/users/:id/edit", to: "users#edit" 
   # Defines the root path route ("/")
   # root "articles#index"
 end
